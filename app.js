fa8f49754f162e5fce028a5199360ea5887bee16
Mohammed Ahmed <mahme029@gold.ac.uk>
2023-07-16 02:07:36 +0100

    solved '<<' token error

--- app.js
+++ app.js
@@ -74,14 +74,9 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-<<<<<<< HEAD
 app.listen(3000, function() {
   console.log("server running on port 3000");
-=======
-app.listen(3000, function () {
-  console.log("Hello new branch");
->>>>>>> update
-});
+})
 
 
 
