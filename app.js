4ec47b9d1d5cd570016b698f62eb64eb30db5718
Mohammed Ahmed <mahme029@gold.ac.uk>
2023-07-16 01:45:48 +0100

    Succesfully connected to database, Also able to delete and add new task to the db

--- app.js
+++ app.js
@@ -3,54 +3,93 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://127.0.0.1:27017/myList');
+
+const listSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    min: 0
+  }
+})
+
+const listModel = new mongoose.model("Events", listSchema)
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = [];
 const workItems = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
-const day = date.getDate();
+  const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const item = req.body.newItem;
+  const newListItem=new listModel({
+    task:item
+  })
+  newListItem.save()
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  items.push(item);
+  res.redirect("/");
+
 });
 
 
-app.post("/remove",(req,res)=>{
-  const delIndex=parseInt(req.body.click)
-  items.splice(delIndex,1)
+app.post("/remove", (req, res) => {
+  let delIndex = parseInt(req.body.click)
+  listModel.deleteOne({"task":items[delIndex]}).then(function(){
+    console.log("succesfully deleted")
+  })
+  items.splice(delIndex, 1)
+
   res.redirect("/")
 
 })
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Hello new branch");
 });
+
+
+
+function pushDataToArr(){
+  listModel.find({}).then(function(res){
+    for(let i=0;i<res.length;i++){
+      items.push(res[i].task)
+    }
+  })
+
+}
+
+
+
+pushDataToArr()
+
